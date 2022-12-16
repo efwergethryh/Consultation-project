3b8ba5f3b40ee5650371858208c1632327af65c7
Mostafa Salam <94201610+efwergethryh@users.noreply.github.com>
2022-12-16 22:43:52 +0300

    Add files via upload

---
+++ 2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+        $table->id();
+            $table->string('name')->nullable();
+            $table->string('email');
+            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->integer('role_id');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+       
+    ;
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
